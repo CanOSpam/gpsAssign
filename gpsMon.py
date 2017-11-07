18b7660ccf4542a120ecb1f04d5f8e818dfe76d9
Tim Bruecker <brooker_113@hotmail.com>
2017-11-06 19:22:58 -0800

    test

--- gpsMon.py
+++ gpsMon.py
@@ -9,28 +9,29 @@ class gpsMon:
         self.gps_socket.connect()
         self.gps_socket.watch()
 
-    def startGps(self):
-        thread = threading.Thread(target=self.gpsWorker)
+
+    def start_gps(self):
+        thread = threading.Thread(target=self.gps_worker)
         thread.start()
 
-    def gpsWorker(self):
+    def gps_worker(self):
         print('Started gps worker')
         for new_data in self.gps_socket:
             if new_data:
                 self.data_stream.unpack(new_data)
-                list_of_satellites = self.data_stream.SKY['satellites']
                 os.system('clear')
+                time = self.data_stream.TPV['time']
+                lat = self.data_stream.TPV['lat']
+                lon = self.data_stream.TPV['lon']
 
-                print(list_of_satellites)
-
-                print('Mode = ', self.data_stream.TPV['mode'], end='')
-                print(' Altitude = ', self.data_stream.TPV['alt'], end='')
-                print(' Latitude = ', self.data_stream.TPV['lat'], end='')
-                print(' Longitude = ', self.data_stream.TPV['lon'])
+                self.gps_data_string = ('Time: ' + str(time) + ' Lat: ' + str(lat) + ' Lon: ' + str(lon))
+                print(self.gps_data_string)
 
                 for sat in self.data_stream.SKY["satellites"]:
-                    print(sat, end='')
-
-                print('')
+                    print("PRN: " + str(sat['PRN']), end='')
+                    print(" Elevation: " + str(sat['el']), end='')
+                    print(" Azimuth: " + str(sat['az']), end='')
+                    print(" SNR: " + str(sat['ss']), end='')
+                    print(" Used: " + str(sat['used']))
 
 
--- main.py
+++ main.py
@@ -1,42 +1,65 @@
 from gpsMon import gpsMon
-from tkinter import Tk, Label, Button
-#from testThread import testThread
-gps = gpsMon()
-#testThread = testThread()
+from tkinter import *
+from tkinter import ttk
+from array import array
+
+
 root = Tk()
 
-def quitNow():
-    root.destroy()
+content = ttk.Frame(root)
+content.grid(column=0, row=0)
+gps_string = StringVar()
+gps = gpsMon()
 
-class MyFirstGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("A simple GUI")
-        self.label = Label(master, text="This is our first GUI!")
-        self.label.pack()
+#Title
+namelbl = ttk.Label(content, text="GPS APP", font=("Helvetica", 64), foreground="red")
+namelbl.grid(column=0, row=0, columnspan=2,pady=10)
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+#Frames
+frame1 = ttk.Frame(content, relief="groove", width=1440, height=100)
+frame2 = ttk.Frame(content, relief="groove", width=1440, height=100)
+frame3 = ttk.Frame(content, relief="groove", width=1440, height=100)
+frame4 = ttk.Frame(content, relief="groove", width=1440, height=100)
+frame5 = ttk.Frame(content, relief="groove", width=1440, height=100)
+frame6 = ttk.Frame(content, relief="groove", width=1440, height=100)
 
-        self.close_button = Button(master, text="Stop Thread", command=self.closeNow)
-        self.close_button.pack()
-
-        #testThread.startThread()
-
-    def greet(self):
-        pass
-        #self.label['text'] = testThread.test
-        #print(testThread.test)
-
-    def closeNow(self):
-        #testThread.stopNow = True
-        quitNow()
+framedata = ttk.Frame(content, relief="groove", width=1440, height=120)
 
 
-my_gui = MyFirstGUI(root)
-gps.startGps()
+frame1.grid(column=0, row=1, rowspan=3, columnspan=2)
+frame2.grid(column=0, row=4, rowspan=3, columnspan=2)
+frame3.grid(column=0, row=7, rowspan=3, columnspan=2)
+frame4.grid(column=0, row=10, rowspan=3, columnspan=2)
+frame5.grid(column=0, row=13, rowspan=3, columnspan=2)
+frame6.grid(column=0, row=16, rowspan=3, columnspan=2)
+
+framedata.grid(column=0, row=19, rowspan=2, columnspan=2)
+
+a = array("i", [1, 2, 3,4,5,6,7,8])
+
+for i in range(0, 6):
+    Label(content, text="PRN: " + "N/A").grid(column=0, row=3*i+1, columnspan=2)
+    Label(content, text="Elevation: " + "N/A").grid(column=0, row=3*i+2)
+    Label(content, text="Azimuth: " + "N/A").grid(column=1, row=3*i+2)
+    Label(content, text="SNR: " + "N/A").grid(column=0, row=3*i+3)
+    Label(content, text="Used: " + "N/A").grid(column=1, row=3*i+3)
+
+namelbl = ttk.Label(content, textvariable=gps_string, font=("Helvetica", 32)).grid(column=0, row=19, columnspan=2, pady=10)
+
+root.resizable(False, False)
+root.geometry('{}x{}'.format(1440, 800))
+gps.start_gps()
+
 while True:
-    #my_gui.label['text'] = testThread.test
+    gps_string.set(gps.gps_data_string)
+    i=0
+    for sat in gps.satellites:
+        Label(content, text="PRN: " + sat['PRN']).grid(column=0, row=3 * i + 1, columnspan=2)
+        Label(content, text="Elevation: " + sat['el']).grid(column=0, row=3 * i + 2)
+        Label(content, text="Azimuth: " + sat['az']).grid(column=1, row=3 * i + 2)
+        Label(content, text="SNR: " + sat['ss']).grid(column=0, row=3 * i + 3)
+        Label(content, text="Used: " + sat['used']).grid(column=1, row=3 * i + 3)
+        i=i+1
     root.update_idletasks()
     root.update()
 
