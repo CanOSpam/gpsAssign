ed1a257c59a97db482e3ccc1e8c1091effbe8fe4
Tim Bruecker <brooker_113@hotmail.com>
2017-11-06 19:54:13 -0800

    dont break pls

--- gpsMon.py
+++ gpsMon.py
@@ -28,14 +28,14 @@ class gpsMon:
                 print(self.gps_data_string)
 
                 for sat in self.data_stream.SKY["satellites"]:
-                    satellite = sat
-                    prn = "PRN: " + str(satellite["PRN"])
+                    print(sat)
+                    #prn = "PRN: " + str(sat["PRN"])
                     #el = sat['el']
                    # az = str(sat['az'])
                    # snr = str(sat['ss'])
                    # used = str(sat['used'])
 
-                    print("PRN: " + prn, end='')
+                    #print("PRN: " + prn, end='')
                     #print(" Elevation: " + el, end='')
                     #print(" Azimuth: " + az, end='')
                     #print(" SNR: " + snr, end='')
